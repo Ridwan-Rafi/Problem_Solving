c5cab732da31ab47b9c21def08ea655a137d4d88
Ridwan-Rafi <70832178+Ridwan-Rafi@users.noreply.github.com>
2022-05-10 07:26:03 +0600

    loj

--- lightOJ/Area of a Parallelogram.cpp
+++ lightOJ/Area of a Parallelogram.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 int main()
 {
-    int t,ax,ay,bx,by,cx,cy,dx,dy,area,x,y;
+    int t,ax,ay,bx,by,cx,cy,dx,dy,x,y;
+    long long int area;
     cin>>t;
     for(int i=1;i<=t;i++)
     {
         cin>>ax>>ay>>bx>>by>>cx>>cy;
         dy=(cy-by)+ay;
         dx=ax+(cx-bx);
-        x=abs(bx-ax);
-        y=abs(cy-by);
-
-        printf("Case %d: %d %d %lld\n",i,dx,dy,x*y);
+area=abs(((ax*by)+(bx*cy)+(cx*dy)+(dx*ay))-((ay*bx)+(by*cx)+(cy*dx)+(dy*ax)));
+area=area*0.5;
+        printf("Case %d: %d %d %lld\n",i,dx,dy,area);
     }
 }
